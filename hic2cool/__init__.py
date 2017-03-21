bac0db07d80d11892c3e9269972d375f4a6165e9
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-03-21 12:42:03 -0400

    0.1.4

--- hic2cool/__init__.py
+++ hic2cool/__init__.py
@@ -1,4 +1,4 @@
-from hic2cool.hic2cool import hic2cool_convert
-from hic2cool._version import __version__
+from hic2cool import hic2cool_convert
+from _version import __version__
 
 __all__ = ["hic2cool_convert", "__version__"]
