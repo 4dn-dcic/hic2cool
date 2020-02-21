5064de0b85da0b22987cebdc4eea21f49d5e9535
Marcel Martin <marcel.martin@scilifelab.se>
2020-02-21 18:05:24 +0100

    Drop setup_requires
    setup_requires dependencies should only list those dependencies that are required by the `setup.py` script itself. This reduces installation time under some circumstances, and will also simplify the Bioconda package.

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
     license = "MIT",
     keywords = ["bioinformatics", "genomics", "hi-c", "juicer", "cooler", "contact-matrix", "file-format"],
     install_requires = requires,
-    setup_requires = requires,
     tests_require = requires,
     test_suite = "test",
     entry_points = {
